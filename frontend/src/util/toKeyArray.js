ab7b5d9dcf2b138ca679a6a3006b2c1194e491e2
gkekals <gkekals12@kbu.ac.kr>
2025-10-31 21:37:18 +0900

    commit

--- frontend/src/util/toKeyArray.js
+++ frontend/src/util/toKeyArray.js
@@ -1,8 +1,5 @@
-
-function toKeyArray(val) {
+export const toKeyArray=(val) =>{
   if (!val) return [];
   const arr = Array.isArray(val) ? val : [val];
   return arr.map(urlToKey).filter(Boolean);
 }
-
-export { toKeyArray };
